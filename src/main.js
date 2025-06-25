57591107942758fa3f6a477233c437fac9c26d77
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2025-06-25 16:04:27 +0500

    gallery, history, fixes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 import './scss/main.scss';
 
-console.log('hello');
+const handleApartmentSelection = () => {
+	const buttons = document.querySelector('.apartments__buttons');
+
+	buttons.addEventListener('click', e => {
+		const btn = e.target.closest('button.apartments__button');
+		if (!btn) return;
+		const lvl = btn.dataset.lvl;
+		const svgGroup = document.querySelector(`.apartments__vector g[data-lvl="${lvl}"]`);
+		document
+			.querySelectorAll('.apartments__vector g')
+			.forEach(g => g.classList.remove('active'));
+		[...buttons.children].forEach(btn => btn.classList.remove('active'));
+		if (!svgGroup) return;
+		svgGroup.classList.add('active');
+		btn.classList.add('active');
+	});
+};
+
+handleApartmentSelection();
