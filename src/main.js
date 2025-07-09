7048466a1bb08c2cdeb97c18bab90cf3e3f06e58
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2025-07-09 20:16:33 +0500

    fixes

--- src/main.js
+++ src/main.js
@@ -1,33 +1,39 @@
 // import './scss/main.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Modal from 'bootstrap/js/dist/modal';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import { register } from 'swiper/element/bundle';
-import { splitAndAnimate, splitElementText } from './js/words';
+import { splitAndAnimate } from './js/words';
 import { GSAPOnce, GSAPScrub } from './js/gsap';
 import Lenis from 'lenis';
 
 gsap.registerPlugin(ScrollTrigger);
 register();
 
-const lenis = new Lenis({ autoRaf: true });
+const lenis = new Lenis({ autoRaf: true, lerp: 0.15 });
 
 const handleApartmentSelection = () => {
-	const buttons = document.querySelector('.apartments__buttons');
-
+	const buttons = document.querySelector('.main .apartments__buttons');
+	const vectors = document.querySelector('.main .apartments__vector');
+	const bsModal = Modal.getOrCreateInstance('#plan-modal');
 	buttons.addEventListener('click', e => {
 		const btn = e.target.closest('button.apartments__button');
 		if (!btn) return;
 		const lvl = btn.dataset.lvl;
-		const svgGroup = document.querySelector(`.apartments__vector g[data-lvl="${lvl}"]`);
+		const svgGroup = document.querySelector(`.main .apartments__vector g[data-lvl="${lvl}"]`);
 		document
-			.querySelectorAll('.apartments__vector g')
+			.querySelectorAll('.main .apartments__vector g')
 			.forEach(g => g.classList.remove('active'));
 		[...buttons.children].forEach(btn => btn.classList.remove('active'));
 		if (!svgGroup) return;
 		svgGroup.classList.add('active');
 		btn.classList.add('active');
+	});
+	vectors.addEventListener('click', e => {
+		const vector = e.target.closest('.apartments__vector-group');
+		if (!vector) return;
+		console.log(vector.dataset.lvl);
+		bsModal.show();
 	});
 };
 const setCopyrightYear = () => {
@@ -42,7 +48,6 @@ const handleMenu = () => {
 
 	const handleToggles = () => {
 		document.body.classList.toggle('no-scroll');
-		document.body.classList.contains('no-scroll') ? lenis.stop() : lenis.start();
 		container.classList.toggle('active');
 		menu.classList.toggle('active');
 		btn.classList.toggle('active');
@@ -83,9 +88,13 @@ const handleFormSubmission = () => {
 	});
 };
 const handlePlanModal = () => {
-	const btn = document.querySelector('.plan__box button');
+	const btn = document.querySelector('.plan__box button.plan--desktop');
 	btn.addEventListener('click', () => {
 		btn.closest('[data-container]').classList.add('show-form');
+	});
+	const planModalEl = document.getElementById('plan-modal');
+	planModalEl.addEventListener('show.bs.modal', () => {
+		document.querySelector('.plan__container').className = 'plan__container';
 	});
 };
 const splitWordsAndAnimate = () => {
@@ -97,7 +106,7 @@ const splitWordsAndAnimate = () => {
 };
 const handleAboutAnimation = () => {
 	const aboutScrollTrigger = {
-		trigger: '.about__title',
+		trigger: window.innerWidth > 768 ? '.about__title' : '.about',
 		start: 'center center',
 		end: '+=1000'
 	};
@@ -108,8 +117,9 @@ const handleAboutAnimation = () => {
 		},
 		scrollTriggerOptions: {
 			...aboutScrollTrigger,
-			pin: true,
+			pin: '.about',
 			pinSpacing: true
+			// pin: true,
 		}
 	});
 	GSAPScrub('.about__image', {
@@ -119,6 +129,7 @@ const handleAboutAnimation = () => {
 		},
 		scrollTriggerOptions: aboutScrollTrigger
 	});
+	GSAPOnce('.about__content', { y: 25 });
 };
 const handleVideoStart = () => {
 	document.querySelectorAll('.section-card').forEach(el => {
@@ -133,12 +144,28 @@ const handleVideoStart = () => {
 const handleHistoryAnimations = () => {
 	const sections = gsap.utils.toArray('.history');
 	// 1) Define your four off-screen start positions (plus optional rotation)
-	const DIRS = [
-		{ x: -300, y: -200, rotation: 15 }, // ↖ from top-left
-		{ x: -500, y: 200, rotation: -15 }, // ↗ from top-right
-		{ x: 300, y: 400, rotation: 10 }, // ↙ from bottom-left
-		{ x: 300, y: -200, rotation: -40 } // ↘ from bottom-right
-	];
+	const getDIRS = () => {
+		const w = window.innerWidth;
+		return [
+			{ x: -0.3 * w, y: -0.2 * w, rotation: 15 }, // ↖ from top-left
+			{ x: -0.5 * w, y: 0.2 * w, rotation: -15 }, // ↗ from top-right
+			{ x: 0.3 * w, y: 0.4 * w, rotation: 10 }, // ↙ from bottom-left
+			{ x: 0.3 * w, y: -0.2 * w, rotation: -40 } // ↘ from bottom-right
+		];
+	};
+	const DIRS = getDIRS();
+	const scrollTriggerObj =
+		window.innerWidth > 900
+			? {
+					start: 'center center',
+					end: '+=1000',
+					scrub: 1,
+					pin: true,
+					pinSpacing: true
+			  }
+			: {
+					start: 'top center'
+			  };
 
 	sections.forEach(section => {
 		const label = section.querySelector('.section-label');
@@ -152,17 +179,13 @@ const handleHistoryAnimations = () => {
 		const tl = gsap.timeline({
 			scrollTrigger: {
 				trigger: section,
-				start: 'top center'
-				// end: '+=1000',
-				// scrub: 1,
-				// pin: true,
-				// pinSpacing: true
+				...scrollTriggerObj
 			}
 		});
 		tl.to(section.querySelector('.section-label'), {
 			yPercent: 0,
 			top: 0,
-			marginTop: 'clamp(15px, 1.1vw, 20px)',
+			marginTop: 'clamp(15px, 2.8vw, 52px)',
 			scale: 1.5
 		});
 		tl.to(section.querySelector('.section-label'), {
@@ -170,7 +193,7 @@ const handleHistoryAnimations = () => {
 			left: 0,
 			scale: 1,
 			color: 'rgba(255, 255, 255, 0.6)',
-			marginLeft: window.innerWidth > 768 ? 'min(1.7vw, 32px)' : 'clamp(15px, 1.1vw, 20px)'
+			marginLeft: 'clamp(15px, 2.8vw, 52px)'
 		});
 		tl.from(
 			section.querySelector('.history__top'),
@@ -178,15 +201,16 @@ const handleHistoryAnimations = () => {
 				opacity: 0,
 				y: 25
 			},
-			'-=0.75'
-		);
-		tl.to(
-			section.querySelector('.section-label__container'),
-			{
-				background: '#FFFFFF1F'
-			},
+			// '-=0.75'
 			'-=0.5'
 		);
+		// tl.to(
+		// 	section.querySelector('.section-label__container'),
+		// 	{
+		// 		background: '#FFFFFF1F'
+		// 	},
+		// 	'-=0.5'
+		// );
 
 		tl.from(
 			section.querySelectorAll('.history__card'),
@@ -231,7 +255,10 @@ const handleBody = () => {
 	const onBodyClassChange = mutationsList => {
 		for (let mutation of mutationsList) {
 			if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-				document.body.className === 'modal-open' ? lenis.stop() : lenis.start();
+				document.body.classList.contains('modal-open') ||
+				document.body.classList.contains('no-scroll')
+					? lenis.stop()
+					: lenis.start();
 			}
 		}
 	};
@@ -249,51 +276,27 @@ const handleBody = () => {
 	// observer.disconnect();
 };
 const handleOtherAnimations = () => {
-	GSAPOnce('.about__content', { x: -50 });
-
 	gsap.utils.toArray('.gallery__top').forEach(el => {
 		GSAPOnce(el.firstElementChild, { y: 25 });
 		GSAPOnce(el.lastElementChild, { y: 25 });
 	});
 
-	const allCards = gsap.utils.toArray('.gallery__card');
 	const allGalleries = gsap.utils.toArray('.gallery__list');
-	const vals = Array.from({ length: allCards.length }, () => ({
-		x: Math.floor(Math.random() * 200) - 100,
-		y: Math.floor(Math.random() * 200) - 100,
-		rotation: Math.floor(Math.random() * 30) - 15
-	}));
 	allGalleries.forEach((el, i) => {
-		[...el.children].forEach((child, index) => {
-			GSAPOnce(
-				child,
-				{
-					y: vals[i].y,
-					x: vals[i].x,
-					rotation: vals[i].rotation,
-					delay: index * 0.1
-				},
-				{
-					trigger: el
-				}
-			);
+		GSAPOnce(el.children, {
+			stagger: 0.1,
+			y: 100
 		});
 	});
-	GSAPOnce('.apartments>*', {
+	GSAPOnce('.container .apartments>*', {
 		scale: 0.9,
 		stagger: 0.1
 	});
-	GSAPOnce(
-		'.cta .form>*',
-		{
-			y: 25,
-			stagger: 0.2
-		},
-		{
-			trigger: '.cta .form'
-		}
-	);
-	GSAPOnce('.footer>*', {
+	GSAPOnce('.cta>*', {
+		y: 25,
+		stagger: 0.2
+	});
+	GSAPOnce('.footer>*:not(.logo-link)', {
 		yPercent: 25,
 		stagger: 0.15
 	});
@@ -304,25 +307,35 @@ const handleSafari = () => {
 	}
 };
 const handleHistoryModal = () => {
-	const slider = document.querySelector('.history-modal__slider');
-	const barElements = document.querySelectorAll('.history-modal__bar--inner');
-	const timeline = gsap.timeline({
-		repeat: -1
-	});
-	setInterval(() => {
-		if (slider.swiper.activeIndex === slider.swiper.slides.length - 1) {
-			slider.swiper.slideTo(0);
-		} else {
-			slider.swiper.slideNext();
-		}
-	}, 3000);
-	barElements.forEach(bar => {
-		timeline.to(bar, {
-			scaleX: 1,
-			duration: 3
+	const sliderEl = document.querySelector('.history-modal__slider');
+	const bars = document.querySelectorAll('.history-modal__bar--inner');
+	const swiper = sliderEl.swiper;
+
+	const updateBars = activeIndex => {
+		bars.forEach((bar, i) => {
+			const isBefore = i < activeIndex;
+			const isCurrent = i === activeIndex;
+
+			// animate filled / empty
+			gsap.to(bar, {
+				scaleX: isBefore ? 1 : 0,
+				duration: 0.5,
+				ease: 'power2.out'
+			});
+
+			// set a class on the current bar for custom styling
+			bar.classList.toggle('current', isCurrent);
 		});
+	};
+
+	// initial paint
+	updateBars(swiper.activeIndex);
+
+	swiper.on('slideChange', () => {
+		updateBars(swiper.activeIndex);
 	});
 };
+
 const handleHeroAnimation = () => {
 	// Hero animation is full CSS here we only toggle body overflow
 	document.body.classList.add('no-scroll');
@@ -331,6 +344,33 @@ const handleHeroAnimation = () => {
 		document.body.classList.remove('no-scroll');
 		lenis.start();
 	}, 3000);
+};
+const handleParallax = () => {
+	gsap.from('.hero__towers img', {
+		yPercent: 10,
+		ease: 'none',
+		scrollTrigger: {
+			scrub: 1,
+			trigger: '.hero__towers',
+			start: 'top 80%',
+			end: 'bottom center'
+		}
+	});
+};
+const handleButtons = () => {
+	const buttons = document.querySelectorAll('.button, .section-card__button');
+	buttons.forEach(button => {
+		const svgIcon = button.querySelector('.icon');
+		if (!svgIcon) return;
+		button.removeChild(svgIcon);
+
+		const box = document.createElement('div');
+		box.classList.add('button__box');
+		box.appendChild(svgIcon);
+		box.appendChild(svgIcon.cloneNode(true));
+
+		button.appendChild(box);
+	});
 };
 
 handleApartmentSelection();
@@ -349,3 +389,5 @@ handleBody();
 handleOtherAnimations();
 handleSafari();
 handleHistoryModal();
+handleParallax();
+handleButtons();
